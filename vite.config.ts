4a6e5ea8231ee2385881e11149c199045f5a976a
fadel-elias <fadel.elias@estudante.ufjf.br>
2025-08-19 20:06:24 -0300

    configura a base no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/ufjf-dcc206-2025-1-a-trb-elias-team/',
   test: {
     // ambiente de teste (jsdom simula um navegador)
     environment: 'jsdom',
